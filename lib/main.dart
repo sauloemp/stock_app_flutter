03fbf31e77ef806074ad85a0692abf2af6cc6e53
Saulo Pereira <46585116+sauloemp@users.noreply.github.com>
2022-04-14 17:13:04 -0300

    Commit just a comment
    Login Screen
    
    Error

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,169 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:stock_app_flutter/widgets/home_page.dart';
 
-void main() => runApp(App());
+void main() {
+  runApp(const MyApp());
+}
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
-class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Stocks",
       home: HomePage(),
     );
   }
 }
+
+/*
+  TODO list:
+  1° Passo: desenvolver o layout do app (apenas o UI)
+  2° Passo: Criar uma conta no fire base
+  3° Passo: Criar um projeto e vincular o firebase
+  4° Passo: Adicionar as firebase dependencies no Flutter
+  5° Passo: Iniciando o Firebase
+  6° Passo: Criar uma função de login
+  */
+
+//Home Page
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+class _HomePageState extends State<HomePage> {
+
+  //Iniciando o Firebase
+  Future<FirebaseApp> _initializeFirebase() async{
+    FirebaseApp firebaseApp = await Firebase.initializeApp();
+    return firebaseApp;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: FutureBuilder(
+        future: _initializeFirebase(),
+        builder: (context, snapshot){
+          if (snapshot.connectionState == ConnectionState.done){
+            return LoginScreen();
+          }
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        },
+      ),
+    );
+  }
+}
+
+//Login Screen
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({Key? key}) : super(key: key);
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+//16 mim
+  //Função de Login
+  static Future<User?> loginUsingEmailPassword({
+    required String email,
+    required String password,
+    required BuildContext context
+  }) async {
+      FirebaseAuth auth = FirebaseAuth.instance;
+      User? user;
+      try{
+        UserCredential userCredential = await auth.signInWithEmailAndPassword(email: email, password: password);
+        user = userCredential.user;
+    } on FirebaseAuthException
+      catch (e){
+        if(e.code == "user-not-found"){
+          print("No User found for that e-mail");
+        }
+      }
+    return user;
+}
+  }
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Text(
+            "Stocks Investment App",
+            style: TextStyle(
+              color: Colors.black,
+              fontSize: 28.0,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          const Text("Login to SI App",
+            style: TextStyle(
+              color: Colors.black,
+              fontSize: 44.0,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          const SizedBox(
+            height: 44.0,
+          ),
+          const TextField(
+            keyboardType: TextInputType.emailAddress,
+            decoration: InputDecoration(
+              hintText: "User E-mail",
+              prefix: Icon(Icons.mail, color: Colors.black),
+            ),
+          ),
+          const SizedBox(
+            height: 26.0,
+          ),
+          const TextField(
+            obscureText: true,
+            decoration: InputDecoration(
+              hintText: "User Password",
+              prefix: Icon(Icons.lock, color: Colors.black),
+            ),
+          ),
+          const SizedBox(
+            height: 12.0,
+          ),
+          const Text(
+            "Don't Remember your Password?",
+            style: TextStyle(
+              color: Colors.black,
+            ),
+          ),
+          const SizedBox(
+            height: 88.0,
+          ),
+          Container(
+            width: double.infinity,
+            child: RawMaterialButton(
+              fillColor: Color(0xFF0069FE),
+              elevation: 0.0,
+              padding: EdgeInsets.symmetric(vertical: 20.0),
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(12.0)),
+              onPressed: (){},
+              child: Text("Login",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 18.0,
+                  )),
+            ),
+          )
+
+        ],
+      ),
+
+    );
+  }
